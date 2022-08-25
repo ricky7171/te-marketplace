16a3ca21be680d462fcd1e6b933c3b83cb976dac
Samuel Ricky Saputro <samuel.ricky@ti.ukdw.ac.id>
2022-08-25 17:29:47 +0700

    fix response struct

--- internal/modules/account/presentation/response.go
+++ internal/modules/account/presentation/response.go
@@ -3,12 +3,12 @@ package accountpresent
 // ini lagi misahin object response. Pokoknya yang dikembaliin oleh app service itu tetep model, bukan loginResponse
 // loginResponse ini dikembaliin oleh handler.
 type LoginResponse struct {
-	Token        string `json:token`
-	RefreshToken string `json:refresh_token`
+	Token        string `json:"token"`
+	RefreshToken string `json:"refresh_token"`
 	User         UserData
 }
 
 type UserData struct {
-	Email     string `json:email`
-	CreatedAt string `json:created_at`
+	Email     string `json:"email"`
+	CreatedAt string `json:"created_at"`
 }
